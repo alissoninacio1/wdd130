cc4f47062a3a2b21c3760352dbb34aface400b4a
Álisson Inácio <69402669+alissoninacio1@users.noreply.github.com>
2022-07-07 18:06:45 -0600

    menu fixed reverse

--- gastrocollab/menu-fixed.js
+++ gastrocollab/menu-fixed.js
@@ -4,8 +4,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("header").style.top = "0";
-  } else {
     document.getElementById("header").style.top = "-400px";
+  } else {
+    document.getElementById("header").style.top = "0";
   }
 }
