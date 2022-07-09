10fe0f1b54dfe73640e6a1313d5ec4e3f3cd8a69
Álisson Inacio <ina21001@byui.edu>
2022-07-09 13:46:31 -0600

    images adjusted

--- gastrocollab/programs/menu-fixed.js
+++ gastrocollab/programs/menu-fixed.js
@@ -3,7 +3,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 45 || document.documentElement.scrollTop > 45) {
     document.getElementById("header").style.top = "0";
   } else {
     document.getElementById("header").style.top = "-400px";
